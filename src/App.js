f804090d97595b4f1f35c73ca9627caa62185b24
masterdevnet <masterdev.net@gmail.com>
2022-03-13 15:17:47 -0300

    Feita conexão com o a API e listagem dos produtos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const getProdutos = async () => {
+    fetch('http://localhost/apijpautocenter/index.php')
+    .then((response) => response.json())
+    .then((responseJson) => (
+      console.log(responseJson)
+    ))
+  }
+
+  useEffect(() => {
+    getProdutos()
+  },[])
+
   return (
     <div>
-      <h1>Listar Peças</h1>
+      <h1>Listar Produtos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Produto</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Data da Compra</th>
+            <th>Estoque</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
